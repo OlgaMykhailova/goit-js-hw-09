072ebedbb2832f5bf673af6ac690443f6d233b74
OlgaMykhailova <rosebud_z@yahoo.com>
2022-08-09 20:48:55 +0300

    added task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+let timerId = null;
 
+buttonStartEl.addEventListener('click', onClickStart);
+buttonStopEl.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  buttonStartEl.toggleAttribute('disabled');
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStop() {
+  clearInterval(timerId);
+  buttonStartEl.toggleAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
