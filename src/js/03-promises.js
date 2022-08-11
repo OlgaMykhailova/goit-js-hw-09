a8065b7b4432d470b213af478bc2a06628c62aa8
OlgaMykhailova <rosebud_z@yahoo.com>
2022-08-11 18:41:47 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -41,7 +41,7 @@ function makePromises(event) {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
     currentDelay += Number(inputStep.value);
   }
